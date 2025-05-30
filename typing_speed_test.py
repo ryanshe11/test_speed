3103081b7950ac1a0d72e1be1a5969bbbe643138
ryanshe11 <Renekvium@protonmail.com>
2025-05-30 21:07:21 +0100

    Update typing_speed_test.py

--- typing_speed_test.py
+++ typing_speed_test.py
@@ -5,7 +5,7 @@ import os
 import random
 
 # List of sample sentences for typing
-sentence = []
+sentence = ['bor']
 sentences = [
     "The quick brown fox jumps over the lazy dog.",
     "Python is a widely used high-level programming language.",
